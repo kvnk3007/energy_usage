3609dcb038dac4abb63eceac7a5fba9cd20fc776
kvnk3007 <kvn.f.kennedy@gmail.com>
2024-10-05 19:05:14 +0100

    added gitleaks with db connection v2

--- run_app.py
+++ run_app.py
@@ -4,7 +4,7 @@ print("hello")
 
 import mysql.connector
 
-cnx = mysql.connector.connect(user='scott', password='password',
-                              host='127.0.0.1',
-                              database='employees')
+cnx = mysql.connector.connect(user="scott", password="S@LtPepp3r@",
+                              host="127.0.0.1",
+                              database="employees")
 cnx.close()
