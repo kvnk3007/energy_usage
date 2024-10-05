22a6400591fc916ed1df0ab23f7edf92574f980f
kvnk3007 <kvn.f.kennedy@gmail.com>
2024-10-05 19:13:31 +0100

    doen with sec changes

--- run_app.py
+++ run_app.py
@@ -2,9 +2,3 @@
 
 print("hello")
 
-import mysql.connector
-
-cnx = mysql.connector.connect(user="scott", password="S@LtPepp3r@",
-                              host="127.0.0.1",
-                              database="employees")
-cnx.close()
